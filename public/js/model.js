00c5ba2474f58dc318b7dcf1122e0cbbef3caf2b
srini102 <srini102@yahoo.com>
2017-07-08 22:38:46 -0400

    fix: Add About tab to the navbar

---
+++ public/js/model.js
@@ -0,0 +1,75 @@
+// Model Definition
+
+// Defining an array of objects with song info.
+var saraliVarisais = [
+		{title: 'Varisai 1', fileSrc: 'sarali/sarali1.html', audioSrc: "static/sarali1.wav", type: 'Sarali varisai'},
+		{title: 'Varisai 2', fileSrc: 'sarali/sarali2.html', audioSrc: "static/sarali2.wav", type: 'Sarali varisai'},
+		{title: 'Varisai 3', fileSrc: 'sarali/sarali3.html', audioSrc: "static/sarali3.wav", type: 'Sarali varisai'},
+		{title: 'Varisai 4', fileSrc: 'sarali/sarali4.html', audioSrc: "static/sarali4.wav", type: 'Sarali varisai'},
+		{title: 'Varisai 5', fileSrc: 'sarali/sarali5.html', audioSrc: "static/sarali5.wav", type: 'Sarali varisai'},
+		{title: 'Varisai 6', fileSrc: 'sarali/sarali6.html', audioSrc: "static/sarali6.wav", type: 'Sarali varisai'},
+		{title: 'Varisai 7', fileSrc: 'sarali/sarali7.html', audioSrc: "static/sarali7.wav", type: 'Sarali varisai'},
+		{title: 'Varisai 8', fileSrc: 'sarali/sarali8.html', audioSrc: "static/sarali8.wav", type: 'Sarali varisai'},
+		{title: 'Varisai 9', fileSrc: 'sarali/sarali9.html', audioSrc: "static/sarali9.wav", type: 'Sarali varisai'},
+		{title: 'Varisai 10-14', fileSrc: 'sarali/sarali10-14.html', audioSrc: "static/sarali10-14.wav", type: 'Sarali varisai'},
+		//{title: 'Varisai 11', fileSrc: 'sarali11.html', audioSrc: "varisai2.wav", type: 'Sarali varisai'},
+		//{title: 'Varisai 12', fileSrc: 'sarali12.html', audioSrc: "varisai2.wav", type: 'Sarali varisai'},
+		//{title: 'Varisai 13', fileSrc: 'sarali13.html', audioSrc: "varisai2.wav", type: 'Sarali varisai'},
+		//{title: 'Varisai 14', fileSrc: 'sarali14.html', audioSrc: "varisai2.wav", type: 'Sarali varisai'},
+		{title: 'Varisai 1', fileSrc: 'jantai/jantai1.html', audioSrc: "static/jantai1.wav", type: 'Jantai varisai'},
+		{title: 'Varisai 2', fileSrc: 'jantai/jantai2.html', audioSrc: "static/jantai2.wav", type: 'Jantai varisai'},
+		{title: 'Varisai 3', fileSrc: 'jantai/jantai3.html', audioSrc: "static/jantai3.wav", type: 'Jantai varisai'},
+		{title: 'Varisai 4', fileSrc: 'jantai/jantai4.html', audioSrc: "static/jantai4.wav", type: 'Jantai varisai'},
+		{title: 'Varisai 5', fileSrc: 'jantai/jantai5.html', audioSrc: "static/jantai5.wav", type: 'Jantai varisai'},
+		{title: 'Varisai 6', fileSrc: 'jantai/jantai6.html', audioSrc: "static/jantai6.wav", type: 'Jantai varisai'},
+		{title: 'Varisai 7', fileSrc: 'jantai/jantai7.html', audioSrc: "static/jantai7.wav", type: 'Jantai varisai'},
+		{title: 'Varisai 8', fileSrc: 'jantai/jantai8.html', audioSrc: "static/jantai8.wav", type: 'Jantai varisai'},
+		{title: 'Varisai 1', fileSrc: 'taatu/taatu1.html', audioSrc: "static/taatu1.wav", type: 'Taatu varisai'},
+		{title: 'Varisai 2', fileSrc: 'taatu/taatu2.html', audioSrc: "static/taatu2.wav", type: 'Taatu varisai'},
+	];
+	
+var geethams = [
+	{title: 'Lambodhara Lakumikara', fileSrc: 'geethams/lambodara.html', audioSrc: 'static/lambodara.wav'},
+	{title: 'Mandara (kundagowra)', fileSrc: 'geethams/mandara.html', audioSrc: 'static/mandara.wav'},
+	{title: 'Vara Veena', fileSrc: 'geethams/varaveena.html', audioSrc: 'static/varaveena.wav'},
+];	
+
+var videos = [
+	{title: 'Paluke Bangaramayena', videoSrc: 'https://www.youtube.com/embed/cmU08XHWnks', language: 'Telugu'},
+	{title: 'Sri Ramachandra Kripalu', videoSrc: 'https://www.youtube.com/embed/MyNSOu-Fl-k?autoplay=1&html5=1', language: 'Sanskrit'},
+	{title: 'Bhaja Govindam', videoSrc: 'https://www.youtube.com/embed/PxXrLNiPvNo?autoplay=1&html5=1', language: 'Sanskrit'},
+	{title: 'Hari Hari Rama Naama', videoSrc: 'https://www.youtube.com/embed/JRF_pMdpZTw?autoplay=1&html5=1', language: 'Telugu'},
+	{title: 'Govardhana Giridhari', videoSrc: '', language: 'Sanskrit'},
+	{title: 'Bhagyada Lakshmi Baramma', videoSrc: 'https://www.youtube.com/embed/4WhkXpC2iqg?autoplay=1&html5=1', language: 'Kannada'},
+	{title: 'Shree Kamakshi', videoSrc: '', language: 'Sanskrit'},
+	{title: 'Seetha Kalyana Vaibhogame', videoSrc: 'https://www.youtube.com/embed/lBVRsXXDJA0?autoplay=1&html5=1', language: 'Tamil'},
+	{title: 'Ye Theruga Nanu Dayachusedavo', videoSrc: 'https://www.youtube.com/embed/2EUysga8pMQ?autoplay=1&html5=1', language: 'Telugu'},
+	{title: 'Darshan Dejo Aaj', videoSrc: '', language: 'Hindi'}
+];
+
+var bhajans = [
+	{title: 'Paluke Bangaramayena', fileSrc: 'static/paluke.pdf', language: 'Telugu', audioSrc:''},
+	{title: 'Sri Ramachandra Kripalu', fileSrc: 'static/shriram.pdf', language: 'Sanskrit', audioSrc:''},
+	{title: 'Bhaja Govindam', fileSrc: 'static/bhajagovindam.pdf', language: 'Sanskrit', audioSrc:''},
+	{title: 'Hari Hari Rama Naama', fileSrc: 'static/harihari.pdf', language: 'Telugu', audioSrc:''},
+	{title: 'Govardhana Giridhari', fileSrc: 'static/govardhana.pdf', language: 'Sanskrit', audioSrc:''},
+	{title: 'Bhagyada Lakshmi Baramma', fileSrc: 'static/bhagyadalakshmi.pdf', language: 'Kannada', audioSrc:''},
+	{title: 'Shree Kamakshi', fileSrc: 'static/ShreeKamakshi.pdf', language: 'Sanskrit', audioSrc:'static/ShreeKamakshi.wav'},
+	{title: 'Seetha Kalyana Vaibhogame', fileSrc: 'static/seethakalyana.pdf', language: 'Tamil', audioSrc:''},
+	{title: 'Ye Theruga Nanu Dayachusedavo', fileSrc: 'static/YeTheeruga.pdf', language: 'Telugu', audioSrc:'static/YeTheeruga.wav'},
+];
+
+var alankarams = [
+{title: 'Chaturashra Jaati Druva Taalam', fileSrc: 'alankaram/druva.html', audioSrc: 'static/druva.wav'},
+{title: 'Chaturashra Jaati Matya TaaLam', fileSrc: 'alankaram/matya.html', audioSrc: 'static/matya.wav'},
+{title: 'Chaturashra Jaati Roopaka Taalam', fileSrc: 'alankaram/roopaka.html', audioSrc: 'static/roopaka.wav'},
+{title: 'Mishra Jaati Jhampa Taalam', fileSrc: 'alankaram/jhampa.html', audioSrc: 'static/jhampa.wav'},
+{title: 'Tishra Jaati Triputa Taalam', fileSrc: 'alankaram/triputa.html', audioSrc: 'static/triputa.wav'},
+{title: 'Khanda Jaati Ata Taalam', fileSrc: 'alankaram/ata.html', audioSrc: 'static/ata.wav'},
+{title: 'Chaturashra Jaati Eka Taalam', fileSrc: 'alankaram/eka.html', audioSrc: 'static/eka.wav'}
+];
+
+
+filters = ['None', 'Tamil', 'Hindi', 'Telugu', 'Kannada', 'Sanskrit'];
+
+varisaiFilter = ['Sarali varisai', 'Jantai varisai', 'Taatu varisai'];
---
+++ public/js/songView.js
@@ -0,0 +1,126 @@
+// View Definition
+
+// Song constructor to create a song object for the list view.
+var Song = function(data) {
+	this.title = ko.observable(data.title);
+	//this.lines = data.lines;
+	this.audioSrc = data.audioSrc;
+	this.fileSrc = data.fileSrc;
+	this.type = data.type;
+	this.showVarisai = ko.observable(true);
+};
+
+var Geetham = function(data) {
+	this.title = ko.observable(data.title);
+	this.fileSrc = data.fileSrc;
+	this.audioSrc = data.audioSrc;
+};
+
+var Bhajan = function(data) {
+	this.title = ko.observable(data.title);
+	this.fileSrc = data.fileSrc;
+	this.language = data.language;
+	this.audioSrc = data.audioSrc;
+	this.showBhajan = ko.observable(true);
+};
+
+var Video = function(data) {
+	this.title = ko.observable(data.title);
+	this.videoSrc = data.videoSrc;
+	this.language = data.language;
+};
+
+var Alankaram = function(data) {
+	this.title = ko.observable(data.title);
+	this.videoSrc = data.videoSrc;
+	this.fileSrc = data.fileSrc;
+};
+
+
+// View Model
+
+var ViewModel = function(){
+	var self = this;
+	this.songList = ko.observableArray([]);
+	saraliVarisais.forEach(function(songItem) {
+		self.songList.push(new Song(songItem));
+	});
+	this.currentSong = ko.observable(this.songList()[0]);
+	this.setSelectedSong = function(currentsong) {
+		self.currentSong(currentsong);
+	};
+	// Define a filter option for the varisai list.
+	// Show and hide the varisais based on the filter.
+	this.varisaiType = ko.observableArray([]);
+	varisaiFilter.forEach(function(filterOption) {
+		self.varisaiType.push(filterOption);
+	});
+	this.selectedType = ko.observable(self.varisaiType()[0]);
+	this.subsetVarisai = ko.computed(function() {
+		var varisaiList = self.songList();
+		//$('.card-title').val = 'Varisai';
+		for(var i=0;i<varisaiList.length;i++) {
+			if(self.selectedType() !== varisaiList[i].type){
+				varisaiList[i].showVarisai(false);
+			} else {
+				varisaiList[i].showVarisai(true);
+			}
+		}
+	});
+	this.geethamList = ko.observableArray([]);
+	geethams.forEach(function(geetham) {
+		self.geethamList.push(new Geetham(geetham));
+	});
+	this.currentGeetham = ko.observable(this.geethamList()[0]);
+	this.setSelectedGeetham = function(currentgeetham) {
+		self.currentGeetham(currentgeetham);
+	};
+	this.videosList = ko.observableArray([]);
+	videos.forEach(function(video) {
+		self.videosList.push(new Video(video));
+	});
+	this.currentVideo = ko.observable(this.videosList()[0]);
+	this.setSelectedVideo = function(currentvideo) {
+		self.currentVideo(currentvideo);
+	};
+	this.removeSrc = function() {
+		$("#video-window").attr('src', '');
+	};
+	this.bhajanList = ko.observableArray([]);
+	bhajans.forEach(function(bhajan) {
+		self.bhajanList.push(new Bhajan(bhajan));
+	});
+	this.currentBhajan = ko.observable(this.bhajanList()[0]);
+	this.setSelectedBhajan = function(currentbhajan) {
+		self.currentBhajan(currentbhajan);
+	};
+	// Define a filter option for the bhajan list.
+	// Show and hide the bhajans based on the filter.
+	this.optionList = ko.observableArray([]);
+	filters.forEach(function(filterOption) {
+		self.optionList.push(filterOption);
+	});
+	this.selectedOption = ko.observable(self.optionList()[0]);
+	this.subsetBhajan = ko.computed(function() {
+		var bhajanList = self.bhajanList();
+		for(var i=0;i<bhajanList.length;i++) {
+			if (self.selectedOption() === 'None') {
+				bhajanList[i].showBhajan(true);
+			} else if(self.selectedOption() !== bhajanList[i].language){
+				bhajanList[i].showBhajan(false);
+			} else {
+				bhajanList[i].showBhajan(true);
+			}
+		}
+	});
+	this.alankaramList = ko.observableArray([]);
+	alankarams.forEach(function(alankaram) {
+		self.alankaramList.push(new Alankaram(alankaram));
+	});
+	this.currentAlankaram = ko.observable(this.alankaramList()[0]);
+	this.setSelectedAlankaram = function(currentalankaram) {
+		self.currentAlankaram(currentalankaram);
+	};
+};
+
+ko.applyBindings(new ViewModel());
